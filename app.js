7721cb0f6470cc62b8304de3c9bcc2ff914b7be4
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-07-17 14:49:31 -0700

    class toggling added

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function showPage() {
-    
+let lessonsLink = document.getElementById('lessons-link');
+let compositionLink = document.getElementById('composition-link');
+let lessons = document.getElementById('lessons');
+let composition = document.getElementById('composition');
+
+
+function showLessons() {
+    lessons.classList.toggle("lessons-container");
 }
+
+function showComposition() {
+    composition.classList.toggle("composition-container");
+}
+
+lessonsLink.addEventListener('click', showLessons);
+compositionLink.addEventListener('click', showComposition);
