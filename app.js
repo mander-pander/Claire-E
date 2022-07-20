54ccef98e7c8bd9836aa8b1044e18d8dcaa4613e
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-07-19 17:49:50 -0700

    positioning of link destinations adjusted, text styling, navbar functionality adjusted

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let mainNav = document.getElementById('main-navbar');
 let lessonsLink = document.getElementById('lessons-link');
 let compositionLink = document.getElementById('composition-link');
+let formLink = document.getElementById("form-link");
 let lessons = document.getElementById('lessons');
 let composition = document.getElementById('composition');
+let form = document.getElementById('form');
 
 function showNavbar() {
     mainNav.classList.toggle("mainNav");
@@ -11,11 +13,22 @@ function showNavbar() {
 
 function showLessons() {
     lessons.classList.toggle("lessons-container");
+    composition.classList.add("composition-container");
+    form.classList.add("form-container");
 }
 
 function showComposition() {
     composition.classList.toggle("composition-container");
+    lessons.classList.add("lessons-container");
+    form.classList.add("form-container");
+}
+
+function showForm() {
+    form.classList.toggle("form-container");
+    lessons.classList.add("lessons-container");
+    composition.classList.add("composition-container");
 }
 
 lessonsLink.addEventListener('click', showLessons);
 compositionLink.addEventListener('click', showComposition);
+formLink.addEventListener('click', showForm);
