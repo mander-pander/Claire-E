975d76dd1c1c80f2bf77e031cd584eb92df35ec4
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-07-20 17:54:19 -0700

    worked on responsiveness and absolute positioning

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 let mainNav = document.getElementById('main-navbar');
 let lessonsLink = document.getElementById('lessons-link');
 let compositionLink = document.getElementById('composition-link');
-let formLink = document.getElementById("form-link");
+let portLink = document.getElementById('port-link');
 let lessons = document.getElementById('lessons');
 let composition = document.getElementById('composition');
-let form = document.getElementById('form');
+
 
 function showNavbar() {
     mainNav.classList.toggle("mainNav");
 }
-
 
 function showLessons() {
     lessons.classList.toggle("lessons-container");
@@ -23,12 +22,5 @@ function showComposition() {
     form.classList.add("form-container");
 }
 
-function showForm() {
-    form.classList.toggle("form-container");
-    lessons.classList.add("lessons-container");
-    composition.classList.add("composition-container");
-}
-
 lessonsLink.addEventListener('click', showLessons);
 compositionLink.addEventListener('click', showComposition);
-formLink.addEventListener('click', showForm);
