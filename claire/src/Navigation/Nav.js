41992e1deb434afebf5bcb81507d231efde22412
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-04 12:51:06 -0700

    navbar collapses on click

--- claire/src/Navigation/Nav.js
+++ claire/src/Navigation/Nav.js
@@ -1,18 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { BsInstagram, BsSpotify, BsYoutube } from "react-icons/bs";
+import { TbBrandTiktok } from "react-icons/tb";
+import { useState } from 'react';
 
 function Navigation(props) {
+  const [expanded, setExpanded] = useState(false);
 
-    function handleClick(location) {
-        props.setLocation(location);
-    };
+  function handleClick(location) {
+    props.setLocation(location);
+    setExpanded(false);
+  };
 
   return (
-    <Navbar bg="white" expand="lg">
+    <Navbar expanded={expanded} bg="white" expand="lg">
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link onClick={() => handleClick('Home')}>Home</Nav.Link>
             <Nav.Link onClick={() => handleClick('About')}>About</Nav.Link>
@@ -21,6 +26,18 @@ function Navigation(props) {
             <Nav.Link onClick={() => handleClick('MusicCatologue')}>Music Catologue</Nav.Link>
             <Nav.Link onClick={() => handleClick('Recordings')}>Recordings</Nav.Link>
             <Nav.Link onClick={() => handleClick('Contact')}>Contact Me</Nav.Link>
+            <Nav.Link href="https://www.instagram.com/themusicofclaire/" target="_blank">
+              <BsInstagram/>
+            </Nav.Link>
+            <Nav.Link>
+              <BsSpotify/>
+            </Nav.Link>
+            <Nav.Link>
+              <BsYoutube/>
+            </Nav.Link>
+            <Nav.Link href="https://www.tiktok.com/@themusicofclaire" target="_blank">
+              <TbBrandTiktok />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
