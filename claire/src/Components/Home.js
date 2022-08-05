66b1b15480fdf22d43e0156a0ce7031fd7063a48
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-05 13:53:02 -0700

    adjusted home layout for mobile and web

--- claire/src/Components/Home.js
+++ claire/src/Components/Home.js
@@ -1,11 +1,15 @@
+import Container from 'react-bootstrap/Container';
 import styles from '../CSS/Home.module.css';
+
 
 const Home = () => {
 
   return (
     <div className={styles.home}>
+      <Container className={styles.textBox}>
         <h1>Claire Eckstein</h1>
         <h3> Composer and Educator</h3>
+      </Container>
     </div>
   )
 }
