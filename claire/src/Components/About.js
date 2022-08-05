afa4e7960da51534ba49083a34f9c21c323a1c01
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-05 13:55:50 -0700

    adjusted about page for web and mobile

--- claire/src/Components/About.js
+++ claire/src/Components/About.js
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div className={styles.page}>
         <img className={styles.banner} src={claire} alt="Claire Eckstein, black and white"/>
-        <Container>
+        <Container className={styles.content}>
           <h1>About Me</h1>
             <p>
               Claire Eckstein (b. 1999) is an Indiana-based composer and educator who writes both acoustic and electronic music. She knew from a young age that she loved music, and began music theory and composition classes at the age of fourteen. After achieving the John Philip Sousa Band Award for her accomplishments in highschool, Eckstein moved on to complete her Bachelor of Arts in Humanities - Music with Honors at Indiana University East. Achieving either the Dean’s List or the Chancellor’s List distinction every semester of her college education, Eckstein was one of the top graduating students in her program. After she graduated, Eckstein began sharing her knowledge with aspiring musicians.
