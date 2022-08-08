158043f99f1df39fbea56909f9fa3c033f170872
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-08 11:36:30 -0700

    started composition page styling

--- claire/src/Components/Composition.js
+++ claire/src/Components/Composition.js
@@ -1,21 +1,22 @@
-import {useState} from 'react';
+import styles from '../CSS/Composition.module.css';
+import Container from 'react-bootstrap/Container';
 
 const Composition = () => {
-    const [isShown, setIsShown] = useState(false);
-
-    function handleClick() {
-        setIsShown(current => !current);
-    };
-
 
   return (
-    <div>
-        <button onClick={handleClick}>Composition</button>
-        {isShown && (
-            <div>
-                <h1>Composition</h1>
-            </div>
-        )}
+    <div className={styles.page}>
+      <Container className={styles.content}>
+        <h1>Composition</h1>
+          <p>
+            The commission fee will be calculated by what instrumentation you would like and how long you would like the piece to be.
+            If you are a film or game studio, Eckstein will negotiate price with you based on how much music you require. Eckstein is more than willing to negotiate prices to allow you to stay on budget.
+            Any revisions are free, as Eckstein is focused on making sure your vision for the piece is realized.
+          </p>
+          <span className={styles.notes}> &#119070; &#9833; &#9834; &#9837;</span>
+          <p>
+            For any and all inquiries, kindly send an email to <u>claireecksteinmusic@gmail.com</u> describing your request.
+          </p>
+      </Container>
     </div>
   )
 }
