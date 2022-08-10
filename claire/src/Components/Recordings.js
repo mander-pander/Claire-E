3d600ba10b7e0b602248fee503b08d35de7eff70
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-09 17:36:18 -0700

    youtube videos added

--- claire/src/Components/Recordings.js
+++ claire/src/Components/Recordings.js
@@ -1,21 +1,51 @@
-import {useState} from 'react';
+import styles from '../CSS/Recordings.module.css';
+import Container from 'react-bootstrap/Container';
 
 const Recordings = () => {
-    const [isShown, setIsShown] = useState(false);
-
-    function handleClick() {
-        setIsShown(current => !current);
-    };
-
-
   return (
-    <div>
-        <button onClick={handleClick}>Recordings</button>
-        {isShown && (
+    <div className={styles.page}>
+        <h1>Recordings</h1>
+          <Container className={styles.content}>
             <div>
-                <h1>Recordings</h1>
+              <h4>Pixelation (2018)</h4>
+              <iframe
+                width="500"
+                height="300"
+                src={`https://www.youtube.com/embed/tGapohHgSR0`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Pixelation"
+              />
             </div>
-        )}
+
+            <div>
+              <h4>The Forest (2019)</h4>
+              <iframe
+                width="500"
+                height="300"
+                src={`https://www.youtube.com/embed/RPk3yM-4e-0`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="The Forest"
+              />
+            </div>
+
+            <div>
+              <h4>Superhero (2020)</h4>
+              <iframe
+                width="500"
+                height="300"
+                src={`https://www.youtube.com/embed/uCrLVkA9G-k`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Pixelation"
+              />
+            </div>
+          </Container>
+
     </div>
   )
 }
