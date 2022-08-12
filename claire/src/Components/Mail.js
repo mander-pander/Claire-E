c27a7695a35d070bfe2fb09627220ff667e5b525
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-12 10:41:58 -0700

    email component created and exported

---
+++ claire/src/Components/Mail.js
@@ -0,0 +1,7 @@
+export default function Mailto({ email, subject, body, ...props }) {
+    return (
+      <a href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>
+        {props.children}
+      </a>
+    );
+}
