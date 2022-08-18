25bb2d95760a6541dba56635acd538afa4465ca4
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-18 14:11:35 -0700

    contact content and styling

--- claire/src/Components/Contact.js
+++ claire/src/Components/Contact.js
@@ -1,21 +1,21 @@
-import {useState} from 'react';
+import styles from '../CSS/Contact.module.css';
+import Container from 'react-bootstrap/Container';
+import Mailto from './Mail';
 
 const Contact = () => {
-    const [isShown, setIsShown] = useState(false);
-
-    function handleClick() {
-        setIsShown(current => !current);
-    };
-
 
   return (
-    <div>
-        <button onClick={handleClick}>Contact Me</button>
-        {isShown && (
-            <div>
-                <h1>Contact Me</h1>
-            </div>
-        )}
+    <div className={styles.page}>
+      <Container className={styles.content}>
+        <h1>Contact</h1>
+        <p>
+          If you are interested in performing a work of Claire Eckstein’s, either one currently available or an original commission, you can contact her at&nbsp;
+          <Mailto email="adgodsey@gmail.com" subject="Composition Inquiry" body="Please describe your request or what you might want more information on:">
+                claireecksteinmusic@gmail.com
+          </Mailto>. Similarly, if you are interested in using one of the digital recordings for commercial or educational purposes, please don’t hesitate to reach out.
+        </p>
+        <span className={styles.notes}> &#119070; &#9833; &#9834; &#9837;</span>
+      </Container>
     </div>
   )
 }
