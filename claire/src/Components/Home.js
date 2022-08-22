e3d1bc3631c796b0af4e8f8321b7c43fa4da5e55
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-08-22 11:05:26 -0700

    adjusting home layout

--- claire/src/Components/Home.js
+++ claire/src/Components/Home.js
@@ -1,14 +1,18 @@
-import Container from 'react-bootstrap/Container';
 import styles from '../CSS/Home.module.css';
+import claire from '../CSS/94245871_853002478499614_407991437506904.webp'
+import Container from 'react-bootstrap/Container';
 
 
 const Home = () => {
 
   return (
     <div className={styles.home}>
-      <Container className={styles.textBox}>
-        <h1>Claire Eckstein</h1>
-        <h3> <i>Composer & Educator</i></h3>
+      <Container className={styles.content}>
+        <div className={styles.textBox}>
+          <h1>Claire Eckstein</h1>
+          <h3> <i>Composer & Educator</i></h3>
+        </div>
+        <img src={claire} alt="Claire Eckstein" />
       </Container>
     </div>
   )
